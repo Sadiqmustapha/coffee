f4d35fad22133ef10eaf544294af7460aee6dd63
Sadiqmustapha <115342247+Sadiqmustapha@users.noreply.github.com>
2023-02-03 15:13:56 -0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,48 @@
+let shopItemsData = [{
+    id: "qidas",
+    name: "casual shirt",
+    price: 30,
+    dsc: "New Gucci shirt",
+    img: "img/baddo.jpg",
+}, {
+    id: "sadq",
+    name: "babes",
+    price: 40,
+    dsc: "New BV",
+    img:" img/udemy.jpg",
+}, {
+    id: "sadi",
+    name: "ladies",
+    price: 80,
+    dsc: "New versace",
+    img: "img/freecode.jpg",
+},
+ {
+    id: "sad",
+    name: "casual drip",
+    price: 50,
+    dsc: "New Balanciaga",
+    img: "img/tolani.jpg",
+},
+{
+    id: "sad",
+    name: "casual drip",
+    price: 50,
+    dsc: "New Balanciaga",
+    img: "img/tolani.jpg",
+},
+{
+    id: "sad",
+    name: "casual drip",
+    price: 50,
+    dsc: "New Balanciaga",
+    img: "img/tolani.jpg",
+},
+{
+    id: "sad",
+    name: "casual drip",
+    price: 50,
+    dsc: "New Balanciaga",
+    img: "img/tolani.jpg"
+}
+];
